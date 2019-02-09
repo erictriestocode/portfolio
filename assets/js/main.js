17bded5f8b89e2e0b4910a9da56d02fad66f40fb
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-02-09 16:56:49 -0600

    added pic

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,5 @@
 
 document.addEventListener("DOMContentLoaded", function(){
     console.log("Look at Mr/Mrs/Ms/Miss hotshot in the console debugging the complete lack of JS in my page!");
+    console.log("I see you Will");
 });
