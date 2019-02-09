e783c28747eb10d660816e38dac79f8b75adcdce
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-02-09 16:05:31 -0600

    favicon, stupid JS, things like that

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+// Why would you use JS in a personal page?
+
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("Look at Mr/Mrs/Ms/Miss hotshot in the console debugging the complete lack of JS in my page!");
+});
